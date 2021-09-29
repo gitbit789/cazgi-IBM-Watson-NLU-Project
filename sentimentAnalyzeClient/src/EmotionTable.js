dc2342aa3afc267b37e9f9d0a819e13fc3187e98
gitbit789 <coderone@nolostsignals.com>
2021-09-29 13:13:02 -0500

    pre-production

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -3,29 +3,26 @@ import './bootstrap.min.css';
 
 class EmotionTable extends React.Component {
     render() {
-        let resObject = Object.entries(this.props.emotions);
-
-        return (
-            <div>
-                {/*You can remove this line and the line below. */}
-                {/* {JSON.stringify(this.props.emotions)} */}
-                <table className="table table-bordered">
-                    <tbody>
-                        {
-                            resObject.map((value) =>
-                                <>
-                                    <tr>
-                                        <td>{value[0]}</td>
-                                        <td>{value[1]}</td>
-                                    </tr>
-                                </>
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
-
+      //Returns the emotions as an HTML table
+      return (  
+        <div>
+          <table className="table table-bordered">
+            <tbody>
+            {
+              Object.entries(this.props.emotions).map(function(mapentry) {
+                return (
+                    <tr>
+                    <td>{mapentry[0]}</td>
+                    <td>{mapentry[1]}</td>
+                    </tr>
+                )
+                })
+            }
+            </tbody>
+          </table>
+          </div>
+          );
+        }
+    
 }
 export default EmotionTable;
